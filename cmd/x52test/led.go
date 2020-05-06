0ed5ddfe34fc2b3cefe9236eb5ea2b5689e81fcb
nirenjan <nirenjan@gmail.com>
2020-05-06 14:52:51 -0700

    Cleanup x52test LED test function

--- cmd/x52test/led.go
+++ cmd/x52test/led.go
@@ -5,6 +5,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"nirenjan.org/saitek-x52/x52"
 )
@@ -19,46 +20,47 @@ func testLED(ctx *x52.Context) error {
 		return nil
 	}
 
-	bar := progressBar("LED State", 81)
+	testLED := func(leds []x52.LED, states []x52.LedState) error {
+		const repeatCount = 2
+		for _, led := range leds {
+			// Build the progressbar text
+			text := fmt.Sprintf("%v ", led)
+			for _, state := range states {
+				text += fmt.Sprintf("%v/", state)
+			}
+			text = strings.TrimRight(text, "/")
+			bar := progressBar(text, repeatCount*len(states))
+
+			// Wait for 1 second before actually starting the test
+			delayMs(1000)
+
+			for i := 0; i < repeatCount; i++ {
+				for _, state := range states {
+					ctx.SetLed(led, state)
+					if err := updateDev(ctx, bar); err != nil {
+						return err
+					}
+					delayMs(250)
+					bar.Add(1)
+				}
+			}
+			delayMs(500)
+		}
+
+		return nil
+	}
 
 	onOffLEDs := []x52.LED{x52.LedFire, x52.LedThrottle}
-	colorLEDs := []x52.LED{x52.LedA, x52.LedB, x52.LedD, x52.LedE, x52.LedT1, x52.LedT2, x52.LedT3, x52.LedPOV, x52.LedClutch}
-
 	onOffStates := []x52.LedState{x52.LedOff, x52.LedOn}
+	if err := testLED(onOffLEDs, onOffStates); err != nil {
+		return err
+	}
+
+	colorLEDs := []x52.LED{x52.LedA, x52.LedB, x52.LedD, x52.LedE, x52.LedT1, x52.LedT2, x52.LedT3, x52.LedPOV, x52.LedClutch}
 	colorStates := []x52.LedState{x52.LedOff, x52.LedRed, x52.LedAmber, x52.LedGreen}
-
-	for _, led := range onOffLEDs {
-		for i := 0; i < 2; i++ {
-			for _, state := range onOffStates {
-				bar.Describe(fmt.Sprintf("%8v %-5v", led, state))
-				ctx.SetLed(led, state)
-				if err := updateDev(ctx, bar); err != nil {
-					return err
-				}
-				delayMs(250)
-				bar.Add(1)
-			}
-		}
-		delayMs(500)
+	if err := testLED(colorLEDs, colorStates); err != nil {
+		return err
 	}
-
-	for _, led := range colorLEDs {
-		for i := 0; i < 2; i++ {
-			for _, state := range colorStates {
-				bar.Describe(fmt.Sprintf("%8v %-5v", led, state))
-				ctx.SetLed(led, state)
-				if err := updateDev(ctx, bar); err != nil {
-					return err
-				}
-				delayMs(250)
-				bar.Add(1)
-			}
-		}
-		delayMs(500)
-	}
-
-	bar.Describe("LED State")
-	bar.Finish()
 
 	return nil
 }
